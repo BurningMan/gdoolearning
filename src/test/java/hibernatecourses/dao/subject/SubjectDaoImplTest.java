a4a4b498f25354b6632bf55164e6428f78f25396
Rodion Shkrobot <Rodion_Shkrobot@epam.com>
2013-11-22 17:30:48 +0200

    hibernate mappings

--- src/test/java/hibernatecourses/dao/subject/SubjectDaoImplTest.java
+++ src/test/java/hibernatecourses/dao/subject/SubjectDaoImplTest.java
@@ -2,6 +2,7 @@ package hibernatecourses.dao.subject;
 
 import hibernatecourses.entity.SubjectEntity;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,20 +21,20 @@ import java.util.List;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/hibernate-context.xml")
-
+@Transactional
 public class SubjectDaoImplTest {
 
     private static final String FIRST_TEST_SUBJECT = "firstTestSubject";
     private static final String SECOND_TEST_SUBJECT = "secondTestSubject";
-    private static SubjectEntity firstTestSubject;
-    private static SubjectEntity secondTestSubject;
+    private SubjectEntity firstTestSubject;
+    private SubjectEntity secondTestSubject;
     @Autowired
     private ApplicationContext applicationContext;
     @Autowired
     private SubjectDao subjectDao;
 
-    @BeforeClass
-    public static void setUp() throws Exception {
+    @Before
+    public void setUp() throws Exception {
         firstTestSubject = new SubjectEntity();
         secondTestSubject = new SubjectEntity();
         firstTestSubject.setName(FIRST_TEST_SUBJECT);
@@ -42,16 +43,16 @@ public class SubjectDaoImplTest {
 
     @Test
     public void testAddSubject() throws Exception {
-        Assert.assertFalse(subjectDao.getAllSubjects().contains(firstTestSubject));
-        subjectDao.addSubject(firstTestSubject);
+        Assert.assertEquals(subjectDao.getAllSubjects().size(),0);
+        addSubject(firstTestSubject);
         Assert.assertTrue(subjectDao.getAllSubjects().contains(firstTestSubject));
     }
 
     @Test
     public void testGetAllSubjects() throws Exception {
-        Assert.assertEquals(subjectDao.getAllSubjects().size(), 0);
-        subjectDao.addSubject(firstTestSubject);
-        subjectDao.addSubject(secondTestSubject);
+        Assert.assertEquals(subjectDao.getAllSubjects().size(),0);
+        addSubject(firstTestSubject);
+        addSubject(secondTestSubject);
         List<SubjectEntity> subjectEntities = subjectDao.getAllSubjects();
         Assert.assertTrue(subjectEntities.contains(firstTestSubject));
         Assert.assertTrue(subjectEntities.contains(secondTestSubject));
@@ -59,11 +60,20 @@ public class SubjectDaoImplTest {
 
     @Test
     public void testDeleteSubject() throws Exception {
-        subjectDao.addSubject(firstTestSubject);
+        addSubject(firstTestSubject);
         Assert.assertTrue(subjectDao.getAllSubjects().contains(firstTestSubject));
-        subjectDao.deleteSubject(firstTestSubject.getId());
+        deleteSubject(firstTestSubject);
         Assert.assertFalse(subjectDao.getAllSubjects().contains(firstTestSubject));
-
-
     }
+
+    @Transactional
+    private void addSubject(SubjectEntity subjectEntity) {
+        subjectDao.addSubject(subjectEntity);
+    }
+
+    @Transactional
+    private void deleteSubject(SubjectEntity subjectEntity) {
+        subjectDao.deleteSubject(subjectEntity.getId());
+    }
+
 }
