054dcdc57c78c5cd7bd34fe90138a91fc58d680e
Mariia_Lukianets <Mariia_Lukianets@epam.com>
2013-11-22 16:59:31 +0200

    dao part1.

--- src/main/java/hibernatecourses/dao/attendance/AttendanceDaoImpl.java
+++ src/main/java/hibernatecourses/dao/attendance/AttendanceDaoImpl.java
@@ -1,7 +1,9 @@
 package hibernatecourses.dao.attendance;
 
 import hibernatecourses.entity.AttendanceEntity;
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,22 +13,34 @@ import java.util.List;
  * Date: 22.11.13
  * Time: 12:44
  */
+
 public class AttendanceDaoImpl implements AttendanceDao {
 
 
     @Autowired
     private org.hibernate.SessionFactory sessionFactory;
 
+    @Transactional
     @Override
     public void addAttendance (AttendanceEntity attendance) {
+        Session session = sessionFactory.getCurrentSession();
+        session.save(attendance);
     }
 
+    @Transactional
     @Override
     public List<AttendanceEntity> getAllAttendances () {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("from AttendanceEntity").list();
     }
 
+    @Transactional
     @Override
     public void deleteAttendance (Integer attendanceId) {
+        Session session = sessionFactory.getCurrentSession();
+        AttendanceEntity attendance = (AttendanceEntity) session.load(AttendanceEntity.class, attendanceId);
+        if (attendance != null) {
+            session.delete(attendance);
+        }
     }
 }
--- src/main/java/hibernatecourses/dao/course/CourseDaoImpl.java
+++ src/main/java/hibernatecourses/dao/course/CourseDaoImpl.java
@@ -1,8 +1,10 @@
 package hibernatecourses.dao.course;
 
 import hibernatecourses.entity.CourseEntity;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,22 +14,34 @@ import java.util.List;
  * Date: 22.11.13
  * Time: 12:44
  */
+
 public class CourseDaoImpl implements CourseDao {
 
 
     @Autowired
     private SessionFactory sessionFactory;
 
+    @Transactional
     @Override
     public void addCourse (CourseEntity course) {
+        Session session = sessionFactory.getCurrentSession();
+        session.save(course);
     }
 
+    @Transactional
     @Override
     public List<hibernatecourses.entity.CourseEntity> getAllCourses () {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("from CourseEntity").list();
     }
 
+    @Transactional
     @Override
     public void deleteCourse (Integer courseId) {
+        Session session = sessionFactory.getCurrentSession();
+        CourseEntity course = (CourseEntity) session.load(CourseEntity.class, courseId);
+        if (course != null) {
+            session.delete(course);
+        }
     }
 }
